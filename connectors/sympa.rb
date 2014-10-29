425a042f99c8ea5d6b05d04cb9f3031739f92ef4
Raphaël Bleuse <raphael@bleuse.net>
2014-10-29 11:46:01 +0100

    Add first version of sympa connector

---
+++ connectors/sympa.rb
@@ -0,0 +1,85 @@
+#! /usr/bin/env ruby
+# encoding: utf-8
+
+
+require 'savon' # SOAP abstraction gem
+
+
+class Sympa
+	extend Savon::Model
+
+
+	class SympaError < StandardError; end
+
+
+	def initialize(wsdl)
+		self.class.client(
+			wsdl: wsdl,
+			namespace: 'urn:sympasoap',
+			raise_errors: false
+		)
+		@session = nil
+	end
+
+	operations :add, :info, :login, :review
+=begin
+	operations :add, :am_i, :authenticate_and_run, :authenticate_remote_app_and_run, :cas_login, :check_cookie, :close_list, :complex_lists, :complex_which, :create_list, :del, :get_user_email_by_cookie, :info, :lists, :login, :review, :signoff, :subscribe, :which
+=end
+
+	def add(listname, email, gecos=nil, quiet=false)
+		response = super(
+			message: {
+				listname: listname,
+				email: email,
+				gecos: gecos,
+				quiet: quiet
+			},
+			cookies: @session
+		)
+		check_response response
+	end
+
+	def info(listname)
+		response = super(
+			message: {listname: listname},
+			cookies: @session
+		)
+		check_response response
+		return response.body[:info_response].values[0]
+	end
+
+	def login(login, passwd)
+		response = super(message: {email: login, password: passwd})
+		check_response response
+		@session = HTTPI::Cookie.new(
+			response.http.headers['set-cookie2']
+		)
+	end
+
+	def review(listname)
+		response = super(
+			message: {listname: listname},
+			cookies: @session
+		)
+		check_response response
+		return response.body[:review_response][:return][:item]
+	end
+
+	def check_response(response)
+		if response.body.key?(:fault)
+			raise SympaError, response.body[:fault][:faultstring]
+		end
+	end
+	private :check_response
+end
+
+if File.identical?(__FILE__, $0)
+	sympa_gresille = Sympa.new('https://listes.gresille.org/sympa/wsdl')
+	sympa_gresille.login(login='raphael@bleuse.net', passwd='6WrynFefjot')
+
+	#sympa_gresille.info(listname='esmug-gucem-discussion')
+
+	esmug_gucem_mails = sympa_gresille.review(listname='esmug-gucem')
+	puts esmug_gucem_mails
+end
+
